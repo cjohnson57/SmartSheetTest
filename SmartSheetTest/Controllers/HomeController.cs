72634fe2159667f5e976a58fab9b5a9da678bdd5
Caleb Johnson <C.Johnson.57@outlook.com>
2019-02-20 13:22:04 -0600

    Add project files.

---
+++ SmartSheetTest/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Smartsheet.Api;
+using Smartsheet.Api.Models;
+using SmartSheetTest.Models;
+
+namespace SmartSheetTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        long sheetid = 3750926323345284;
+        string token = "ikow2tj0gtan8l5vha5vsyadzu";
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            ErrorReport er = new ErrorReport();
+            try
+            {
+                int zero = 0;
+                int a = 10 / zero;
+            }
+            catch (Exception e)
+            {
+                er.guid = Guid.NewGuid();
+                er.timestamp = DateTime.Now;
+                er.errormessage = e.ToString();
+            }
+            return View(er);
+        }
+
+        [HttpPost]
+        public ActionResult Form([Bind(Include ="")] ErrorReport er)
+        {
+            AddErrorReportToSmartSheet(er);
+            return View(er);
+        }
+
+        public void AddErrorReportToSmartSheet(ErrorReport er)
+        {
+            SmartsheetClient sheeit = new SmartsheetBuilder().SetAccessToken(token).Build();
+            Row r = new Row();
+            r.ToBottom = true;
+            PaginatedResult<Column> cols = sheeit.SheetResources.ColumnResources.ListColumns(sheetid, null, null);
+            Cell[] cells = new Cell[]
+            {
+                new Cell
+                {
+                    ColumnId = cols.Data.First(x => x.Title == "ID").Id,
+                    Value = er.guid
+                },
+                new Cell
+                {
+                    ColumnId = cols.Data.First(x => x.Title == "TimeStamp").Id,
+                    Value = er.timestamp
+                },
+                new Cell
+                {
+                    ColumnId = cols.Data.First(x => x.Title == "Exception Message").Id,
+                    Value = er.errormessage
+                },
+                new Cell
+                {
+                    ColumnId = cols.Data.First(x => x.Title == "User Message").Id,
+                    Value = er.usermessage
+                },
+            };
+            r.Cells = cells;
+            
+            sheeit.SheetResources.RowResources.AddRows(sheetid, new Row[] { r });
+            Sheet sheet = sheeit.SheetResources.GetSheet(sheetid, null, null, null, null, null, null, null);
+            var row = sheet.Rows.OrderByDescending(x => x.CreatedAt).First();
+            if (er.file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(er.file.FileName);
+                var path = Path.Combine(Server.MapPath("~/App_Data/"), DateTime.Now.ToString("HH-mm-ss") + "-" + fileName);
+                er.file.SaveAs(path);
+                Attachment attachment = sheeit.SheetResources.RowResources.AttachmentResources.AttachFile(sheetid, (long)row.Id, path, "application/jpg");
+                System.IO.File.Delete(path);
+            }
+
+            
+        }
+    }
+}
---
+++ SmartSheetTest/Models/ErrorReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartSheetTest.Models
+{
+    public class ErrorReport
+    {
+        public Guid guid { get; set; }
+        public DateTime timestamp { get; set; }
+        public string errormessage { get; set; }
+        public string usermessage { get; set; }
+        public HttpPostedFileBase file { get; set; }
+    }
+}
